fac9729f056ea4ea499d8d9e503d60c327f866a6
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2024-07-20 17:38:13 +0100

    added files

---
+++ header.php
@@ -0,0 +1,35 @@
+  <head>
+    <!-- Meta Tags -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="keywords" content="xTradeSecurity, digital assets exchange, cryptocurrency trading, crypto wallet, blockchain, trading platform, secure crypto exchange, bitcoin, ethereum, altcoins, crypto trading tools, crypto security">
+    <meta name="description" content="xTradeSecurity is a cutting-edge digital assets exchange, wallet, and trading platform offering secure and user-friendly services for trading a wide range of digital assets. Join us to experience advanced trading tools and robust security features.">
+    <meta name="author" content="xTradeSecurity">
+
+    <!-- Open Graph Meta Tags -->
+    <meta property="og:title" content="xTradeSecurity - Digital Assets Exchange, Wallet, and Trading Platform">
+    <meta property="og:description" content="Discover xTradeSecurity, a new platform for trading digital assets with top-notch security and advanced trading tools.">
+    <meta property="og:image" content="https://example.com/path-to-your-image.jpg">
+    <meta property="og:url" content="assets/images/logo/favicon.png">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="xTradeSecurity">
+
+    <!-- Twitter Card Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="xTradeSecurity - Digital Assets Exchange, Wallet, and Trading Platform">
+    <meta name="twitter:description" content="Join xTradeSecurity to trade a wide range of cryptocurrencies with unparalleled security and advanced trading features.">
+    <meta name="twitter:image" content="assets/images/logo/favicon.png">
+    <meta name="twitter:site" content="@xTradeSecurity">
+
+    <title>xTradeSecurity | Digital Assets Exchange, Wallet, and Trading Platform</title>
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="app/dist/app.css" />
+    <link rel="stylesheet" href="app/dist/magnific-popup.css" />
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <!-- End Style CSS -->
+
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png" />
+    <link rel="apple-touch-icon-precomposed" href="assets/images/logo/favicon.png" />
+  </head>
